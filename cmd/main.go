b44fe884640b1ef4bb193f29144226e50d40dd66
erbol.daurenyli@gmail.com <erbol.daurenyli@gmail.com>
2021-02-07 22:33:31 +0600

    add mapping

--- cmd/main.go
+++ cmd/main.go
@@ -5,8 +5,10 @@ import (
 )
 
 func main() {
+	handlers := new(handler.Handler)
+
 	srv := new(todo.Server)
-	if err := srv.Run("8080"); err != nil {
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occurred while running http server: %s", err.Error())
 	}
 }
--- todo.go
+++ todo.go
@@ -9,3 +9,16 @@ type UsersList struct {
 	UserId int
 	ListId int
 }
+
+type TodoItem struct {
+	Id          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Done        bool   `json:"done"`
+}
+
+type ListsItem struct {
+	Id     int
+	ListId int
+	ItemId int
+}
