d0c21d4476a35074257420687433c1bdf6ac7405
erbol.daurenyli@gmail.com <erbol.daurenyli@gmail.com>
2021-02-25 23:58:52 +0600

    add some features

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,26 @@ import (
 	"github.com/Erbolchik/Golang-todo-rest-api/pkg/handler"
 	"github.com/Erbolchik/Golang-todo-rest-api/pkg/repository"
 	"github.com/Erbolchik/Golang-todo-rest-api/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occurred while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
