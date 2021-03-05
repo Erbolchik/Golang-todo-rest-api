18b6511942f01d0ddeb4af317d2daf4e44534fb2
erbol.daurenyli@gmail.com <erbol.daurenyli@gmail.com>
2021-03-05 23:57:18 +0600

    add sign up

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -1,9 +1,18 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
 
-func (h *Handler) signUp(c *gin.Context) {
+	todo "github.com/Erbolchik/Golang-todo-rest-api"
+	"github.com/gin-gonic/gin"
+)
 
+func (h *Handler) signUp(c *gin.Context) { //registration
+	var input todo.User
+
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(http.StatusBadRequest, err.Error())
+	}
 }
 
 func (h *Handler) signIn(c *gin.Context) {
--- user.go
+++ user.go
@@ -1,8 +1,8 @@
 package todo
 
 type User struct {
-	Id       int    `json:"-"`
-	Name     string `json:"name"`
-	UserName string `json:"username"`
-	Password string `json:"password"`
+	ID       int    `json:"-"`
+	Name     string `json:"name" binding:"required"`
+	UserName string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
