f9b44b4906e45e128672b674fafc9fb558fa5182
erbol.daurenyli@gmail.com <erbol.daurenyli@gmail.com>
2021-03-18 23:54:54 +0600

    add some logic

--- pkg/handler/list.go
+++ pkg/handler/list.go
@@ -3,10 +3,16 @@ package handler
 import "github.com/gin-gonic/gin"
 
 func (h *Handler) createList(c *gin.Context) {
- id,_ c.Get(userCtx)
- c.JSON(http.StatusOK,map[string]interface{}{
-	 "id":id
- })
+	id,ok:=c.Get(userCtx)
+	if !ok{
+		newErrorResponse(c,http.StatusInternalServerError,"user id not found")
+		return
+	}
+	var input todo.TodoList
+	if(err):c.BindJson(&input);err!=nil{
+		newErrorResponse(c,http.StatusInternalServerError,err.error())
+		return
+	}
 }
 func (h *Handler) getAllLists(c *gin.Context) {
 }
