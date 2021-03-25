4f32791ac52883d35e081d8afb0f980a151f5058
erbol.daurenyli@gmail.com <erbol.daurenyli@gmail.com>
2021-03-25 23:26:44 +0600

    todo list transaction

--- pkg/repository/todo_list_postgres.go
+++ pkg/repository/todo_list_postgres.go
@@ -7,3 +7,13 @@ type TodoListPostgres struct {
 func NewTodoListPostgres (db *sqlx.DB) *TodoListPostgres{
 	return &TodoListPostgres{db:db}
 }
+
+func (r *TodoListPostgres) Create(userId int,list todo.TodoList) (int,error){
+	tx,err := r.db.Begin()
+	if err!=nil{
+		return 0,err
+	}
+	var id int
+	createListQuery :=fmt.Sprintf("INSERT INTO %s (title,description) VALUES ($1,$2) RETURNING id",todoListsTable)
+	row:=tx.QueryRow(createListQuery,list.Title,list.Description)
+}
