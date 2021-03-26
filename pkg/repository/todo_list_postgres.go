1d53971d2823220b79c9d2b676ad58bca80e05cf
erbol.daurenyli@gmail.com <erbol.daurenyli@gmail.com>
2021-03-26 23:55:58 +0600

    fix

--- pkg/repository/todo_list_postgres.go
+++ pkg/repository/todo_list_postgres.go
@@ -14,6 +14,19 @@ func (r *TodoListPostgres) Create(userId int,list todo.TodoList) (int,error){
 		return 0,err
 	}
 	var id int
-	createListQuery :=fmt.Sprintf("INSERT INTO %s (title,description) VALUES ($1,$2) RETURNING id",todoListsTable)
-	row:=tx.QueryRow(createListQuery,list.Title,list.Description)
+	createListQuery := fmt.Sprintf("INSERT INTO %s (title, description) VALUES ($1, $2) RETURNING id", todoListsTable)
+	row := tx.QueryRow(createListQuery, list.Title, list.Description)
+	if err := row.Scan(&id); err != nil {
+		tx.Rollback()
+		return 0, err
+	}
+
+	createUsersListQuery := fmt.Sprintf("INSERT INTO %s (user_id, list_id) VALUES ($1, $2)", usersListsTable)
+	_, err = tx.Exec(createUsersListQuery, userId, id)
+	if err != nil {
+		tx.Rollback()
+		return 0, err
+	}
+
+	return id, tx.Commit()
 }
